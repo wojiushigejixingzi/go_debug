b945fe3f0f8025cec8eb05e5bec42e14ffde4457
838206954@qq.com <838206954@qq.com>
2024-05-05 17:17:00 +0800

    合并领个有序链表-递归+迭代

--- test/lt_test.go
+++ test/lt_test.go
@@ -312,3 +312,42 @@ func getIntersectionNode(heaA, headB *ListNode) *ListNode {
 	}
 	return pa
 }
+
+//合并两个有序链表---递归
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list2 == nil {
+		return list1
+	}
+	if list1 == nil {
+		return list2
+	}
+	if list1.Val <= list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
+
+//合并领个有序链表---迭代
+func mergeTwoLists1(list1, list2 *ListNode) *ListNode {
+	prehead := &ListNode{}
+	result := prehead
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			result.Next = list1
+			list1 = list1.Next
+		} else {
+			result.Next = list2
+			list2 = list2.Next
+		}
+		result = result.Next
+	}
+	if list1 != nil {
+		result.Next = list1
+	} else {
+		result.Next = list2
+	}
+	return prehead.Next
+}
